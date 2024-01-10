86061006ec4d9b1ec198995167ab52019f5ab64d
sunny-potato <sun_hee0210@naver.com>
2024-01-10 05:27:18 +0100

    svg animation on main page

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -6,24 +6,21 @@ import MemoryImages from "../Components/Animation/MemoryImages";
 
 function Home() {
   const { userId } = useContext(UserContext);
+
   return (
     <>
-      {userId ? (
-        <div style={{ padding: "100px" }}>Home with logined user</div>
-      ) : (
-        <div className={s.pageContainer}>
-          <div className={s.contentContainer}>
-            <div className={s.pageTitle}>
-              <div>Keep</div>
-              <div>your memories</div>
-              <div>alive</div>
-            </div>
-            <div className={s.pageImage}>
-              <MemoryImages />
-            </div>
+      <div className={s.pageContainer}>
+        <div className={s.contentContainer}>
+          <div className={s.pageTitle}>
+            <div>Keep</div>
+            <div>your memories</div>
+            <div>alive</div>
+          </div>
+          <div className={s.pageImage}>
+            <MemoryImages />
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
